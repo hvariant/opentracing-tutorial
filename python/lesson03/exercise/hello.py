73cba1a93521b9c02f354091fad92cead575ff15
Zhansong Li <zhansong.li@ir.com>
2020-02-13 11:20:27 +1100

    lesson 03

--- python/lesson03/exercise/hello.py
+++ python/lesson03/exercise/hello.py
@@ -2,6 +2,8 @@ import requests
 import sys
 import time
 from lib.tracing import init_tracer
+from opentracing.ext import tags
+from opentracing.propagation import Format
 
 
 def say_hello(hello_to):
@@ -23,10 +25,18 @@ def print_hello(hello_str):
 
 def http_get(port, path, param, value):
     url = 'http://localhost:%s/%s' % (port, path)
-    r = requests.get(url, params={param: value})
+
+    span = tracer.active_span
+    span.set_tag(tags.HTTP_METHOD, 'GET')
+    span.set_tag(tags.HTTP_URL, url)
+    span.set_tag(tags.SPAN_KIND, tags.SPAN_KIND_RPC_CLIENT)
+    headers = {}
+    tracer.inject(span, Format.HTTP_HEADERS, headers)
+
+    r = requests.get(url, params={param: value}, headers=headers)
     assert r.status_code == 200
     return r.text
-        
+
 
 # main
 assert len(sys.argv) == 2
